a84676df562a8559db6a284f531c76ffdb042ed6
晏洋川 <aicola@yanyangchuandeMacBook-Air.local>
2022-06-26 09:53:59 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant, { Form, Field } from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+
+Vue.use(Form)
+Vue.use(Field)
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
